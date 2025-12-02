9f3cc6719f6630f7f6a06c04b1cd8f33dec22173
Mike Gardner <mjgardner@abitofhelp.com>
2025-12-02 01:03:56 -0700

    chore: set for release 1.0.0

--- config/tzif_config.ads
+++ config/tzif_config.ads
@@ -5,7 +5,7 @@ pragma Style_Checks (Off);
 package Tzif_Config is
    pragma Pure;
 
-   Crate_Version : constant String := "1.0.1";
+   Crate_Version : constant String := "1.0.0";
    Crate_Name : constant String := "tzif";
 
    Alire_Host_OS : constant String := "macos";
